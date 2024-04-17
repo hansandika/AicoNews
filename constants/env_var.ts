9f89e51013c8a5600aadb29682419d0aa7f79874
hans geovani andika <hansandika70@gmail.com>
2024-04-18 00:46:46 +0700

    add environment variable

--- constants/env_var.ts
+++ constants/env_var.ts
@@ -1,12 +1,14 @@
 import "dotenv/config";
 
+export const ENV = process.env.ENV as string;
+
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
 
 export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET as string;
 export const NEXTAUTH_URL = process.env.NEXTAUTH_URL as string;
 
-export const DATABASE_URL = process.env.DATABASE_URL as string;
+export const DATABASE_URL = ENV === "development" ? process.env.DATABASE_URL : process.env.DATABASE_URL_PROD as string;
 
 export const CHROMADB_HOST = process.env.CHROMADB_HOST as string;
 export const CHROMADB_COLLECTION_NAME = process.env.CHROMADB_COLLECTION_NAME as string;
