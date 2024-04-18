0cd6aa29fcb806da39259118f21e1882e406ba79
hans geovani andika <hansandika70@gmail.com>
2024-04-18 11:44:09 +0700

    Fix eslint and add chroma db env variable

--- lib/validators/message.ts
+++ lib/validators/message.ts
@@ -1,14 +1,14 @@
 import { z } from 'zod'
 
 export const MessageSchemaValidator = z.object({
-  content: z.string().min(1),
-  role: z.enum(['user', 'assistant'])
+	content: z.string().min(1),
+	role: z.enum(['user', 'assistant'])
 })
 
 
 export const ChatValidator = z.object({
-  messages: z.array(MessageSchemaValidator),
-  languageStyle: z.enum(['formal', 'casual'])
+	messages: z.array(MessageSchemaValidator),
+	languageStyle: z.enum(['formal', 'casual'])
 })
 
 export const QuerySchemaValidator = z.string().min(1)
--- lib/validators/user.ts
+++ lib/validators/user.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod'
 
 export const UserSchemaValidator = z.object({
-  id: z.string(),
-  name: z.string().optional(),
-  email: z.string().email().optional(),
-  image: z.string().optional(),
-  emailVerified: z.date().optional(),
+	id: z.string(),
+	name: z.string().optional(),
+	email: z.string().email().optional(),
+	image: z.string().optional(),
+	emailVerified: z.date().optional(),
 })
